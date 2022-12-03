088a25ce7c6c6312bafc5e2ee65c128f8da01ed5
Ralf Baussenwein <ralf@bwein.net>
2022-12-03 14:40:27 +0100

    Refactor deprecations + CS

--- src/Controller/ContentElement/BweinDismissableBannerElementController.php
+++ src/Controller/ContentElement/BweinDismissableBannerElementController.php
@@ -12,33 +12,34 @@ declare(strict_types=1);
 
 namespace Bwein\DismissableBannerElement\Controller\ContentElement;
 
-use Contao\Config;
 use Contao\ContentModel;
 use Contao\CoreBundle\Asset\ContaoContext;
 use Contao\CoreBundle\Controller\ContentElement\AbstractContentElementController;
-use Contao\CoreBundle\Framework\FrameworkAwareInterface;
-use Contao\CoreBundle\Framework\FrameworkAwareTrait;
+use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\CoreBundle\Routing\ScopeMatcher;
 use Contao\CoreBundle\ServiceAnnotation\ContentElement;
 use Contao\StringUtil;
 use Contao\Template;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @ContentElement("dismissableBanner", category="banner", template="ce_dismissable_banner")
  */
-class BweinDismissableBannerElementController extends AbstractContentElementController implements FrameworkAwareInterface
+class BweinDismissableBannerElementController extends AbstractContentElementController
 {
-    use FrameworkAwareTrait;
+    private ContaoFramework $framework;
+    private ScopeMatcher $scopeMatcher;
+    private ContaoContext $assetsContext;
+    private ParameterBagInterface $params;
 
-    private $scopeMatcher;
-    private $assetsContext;
-
-    public function __construct(ScopeMatcher $scopeMatcher, ContaoContext $assetsContext)
+    public function __construct(ContaoFramework $framework, ScopeMatcher $scopeMatcher, ContaoContext $assetsContext, ParameterBagInterface $params)
     {
+        $this->framework = $framework;
         $this->scopeMatcher = $scopeMatcher;
         $this->assetsContext = $assetsContext;
+        $this->params = $params;
     }
 
     public function __invoke(Request $request, ContentModel $model, string $section, array $classes = null): Response
@@ -67,15 +68,13 @@ class BweinDismissableBannerElementController extends AbstractContentElementCont
     {
         $params = ['class' => 'ce_text'];
 
-        $model->text = StringUtil::toHtml5($model->text);
-
         // Add the static files URL to images
         if ($staticUrl = $this->assetsContext->getStaticUrl()) {
-            $path = Config::get('uploadPath').'/';
-            $model->text = str_replace(' src="'.$path, ' src="'.$staticUrl.$path, $model->text);
+            $path = $this->params->get('contao.upload_path').'/';
+            $model->text = str_replace(' src="'.$path, ' src="'.$staticUrl.$path, (string) $model->text);
         }
 
-        $params['text'] = StringUtil::encodeEmail($model->text);
+        $params['text'] = StringUtil::encodeEmail((string) $model->text);
         $template->textParams = $params;
     }
 
@@ -90,7 +89,7 @@ class BweinDismissableBannerElementController extends AbstractContentElementCont
         if (0 === strncmp($model->url, 'mailto:', 7)) {
             $model->url = StringUtil::encodeEmail($model->url);
         } else {
-            $model->url = ampersand($model->url);
+            $model->url = StringUtil::ampersand($model->url);
         }
 
         if ($model->rel) {
